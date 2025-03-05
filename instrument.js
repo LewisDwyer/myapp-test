480e5e90847a8545c9bbef476fef0b1c64108915
LewisDwyer <lewisdwyer@gmail.com>
2025-03-05 14:21:39 +0000

    Sentry config

--- instrument.js
+++ instrument.js
@@ -3,33 +3,14 @@ const Sentry = require("@sentry/node");
 const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
-  dsn: "https://ed628432a960caf1a7c56464cb78f8f1@o4508920053563392.ingest.de.sentry.io/4508920063590480",
-  // Custom tracer function for a Node.js Express app
-  tracesSampler: ({ name, attributes, parentSampled }) => {
-    const userAgent = attributes?.["http.user_agent"];
-
-    if (
-      typeof userAgent === "string" &&
-      userAgent.includes("SentryUptimeBot")
-    ) {
-      // Sample 100% of spans from SentryUptimeBot
-      return 1;
-    }
-
-    // Sample 50% of other spans
-    return 0.5;
-  },
+  dsn: "https://e5ae49b092d3568c0e56c1eeb58ff6f9@o4508924865019904.ingest.de.sentry.io/4508924867641424",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  profilesSampleRate: 1.0, // Capture 100% of the profiles
 });
-
-// Sentry.init({
-//   dsn: "https://e5ae49b092d3568c0e56c1eeb58ff6f9@o4508924865019904.ingest.de.sentry.io/4508924867641424",
-//   integrations: [
-//     nodeProfilingIntegration(),
-//   ],
-//   // Tracing
-//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
-//   profilesSampleRate: 1.0, // Capture 100% of the profiles
-// });
 
 // Capture unhandled promise rejections and uncaught exceptions
 process.on('unhandledRejection', (reason, promise) => {
