a21a5da21259580f7ab0f4b1ed1ce8d408946e84
LewisDwyer <lewisdwyer@gmail.com>
2025-03-10 15:07:22 +0000

    removing http integration

--- instrument.js
+++ instrument.js
@@ -6,7 +6,7 @@ Sentry.init({
   dsn: "https://e5ae49b092d3568c0e56c1eeb58ff6f9@o4508924865019904.ingest.de.sentry.io/4508924867641424",
   integrations: [
     nodeProfilingIntegration(),
-    Sentry.httpIntegration(),
+    //Sentry.httpIntegration(),
   ],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
